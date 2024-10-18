f9fa5e16a4ae4b006f28d6e7c8ee011246fe0256
omr50 <omrab4@gmail.com>
2024-10-18 05:17:05 -0400

    Open capture dev, wrote Berkeley packet filters

--- src/main.cpp
+++ src/main.cpp
@@ -31,4 +31,76 @@ int main()
     }
 
     printf("Working?\n");
+
+    int dev_num;
+    printf("Enter the interface number (1-%d): ", i);
+    scanf("%d", &dev_num);
+
+    if (dev_num < 1 || dev_num > i)
+    {
+        fprintf(stderr, "Invalid device number.\n");
+        pcap_freealldevs(devices);
+        exit(1);
+    }
+
+    // get the selected device
+    pcap_if_t *d;
+    for (d = devices, i = 1; i < dev_num; d = d->next, i++)
+        ;
+
+    printf("Selected device: %s\n", d->name);
+
+    pcap_t *handle;
+
+    // Open the device
+    // max size = 65536 to ensure we can capture all packets correctly
+    // promiscuous mode = 1 = true (sniff all packets)
+    // timeout in milliseconds = 1000
+    // error buffer
+    handle = pcap_open_live(d->name, 65536, 1, 1000, errbuff);
+    printf("handle opened successfully!\n");
+
+    if (handle = NULL)
+    {
+        fprintf(stderr, "Couldn't open device %s: %s\n", d->name, errbuff);
+        pcap_freealldevs(devices);
+        exit(1);
+    }
+
+    // create bpf program to indicate what we want to capture
+    struct bpf_program fp;
+    char filter_exp[] = "port 8000"; // filter expression
+    bpf_u_int32 net = 0, mask = 0;
+
+    // get network address and netmask (extra info but not needed)
+    if (pcap_lookupnet(d->name, &net, &mask, errbuff) == -1)
+    {
+        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", d->name, errbuff);
+        net = 0;
+        mask = 0;
+    }
+    printf("network lookup worked!\n");
+
+    // compile filter (why does this take in the handle if we need to again use handle in the set filter
+    // function below)
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+    {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        pcap_freealldevs(devices);
+        pcap_close(handle);
+        exit(1);
+    }
+
+    printf("Compiled bpf successfully\n");
+
+    // Set the filter
+    if (pcap_setfilter(handle, &fp) == -1)
+    {
+        fprintf(stderr, "Couldn't install iflter %s: %s\n", filter_exp, pcap_geterr(handle));
+        pcap_freealldevs(devices);
+        pcap_close(handle);
+        exit(1);
+    }
+
+    pcap_freecode(&fp); // free compiled filter
 }
