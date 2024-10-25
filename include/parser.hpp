04df5531d29a7e88ea2998aa9eacd998c352c618
omr50 <omrab4@gmail.com>
2024-10-24 23:57:18 -0400

    Determine packet process:

--- include/parser.hpp
+++ include/parser.hpp
@@ -9,6 +9,24 @@ pointer to packet data          packet classes eth -> ip -> tcp -> http (etc.)
 */
 #pragma once
 
+#include <memory>
+#include "packet.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/ether.h>
+#include <arpa/inet.h>
+#include <pcap/pcap.h>
+
 class Parser
 {
+
+public:
+        // Determine class type and return a pointer to it
+        std::weak_ptr<Packet> Determine_Packet(pcap_pkthdr *header, const u_char *packet)
+        {
+                // use the header to determine packet, create packet, allow packet
+                // to parse its own headers. Then continue on to the next packet.
+                // Also another function will be responsible for linking all the
+                // packets together.
+        }
 };
