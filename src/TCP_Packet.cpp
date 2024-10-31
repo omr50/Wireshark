d8107c56ab9218a6f4b55df9431581012dee784c
omr50 <omrab4@gmail.com>
2024-10-30 21:01:31 -0400

    Checking for http and https

--- src/TCP_Packet.cpp
+++ src/TCP_Packet.cpp
@@ -10,15 +10,20 @@ TCP_Packet::TCP_Packet(const u_char *data, size_t length, timeval time_stamp)
 
 void TCP_Packet::parse()
 {
-    // 1 byte so no need to conveert to little endian
-    if (ntohs(this->tcp_hdr->th_dport) == 80)
+    uint16_t dst_port = ntohs(this->tcp_hdr->th_dport);
+    // http can also be on other ports (have to modify this later)
+    if (dst_port == 80)
     {
         // CREATE ICMP PACKET
         printf("THIS IS HTTP PORT IS 80!\n");
     }
-    else if ((this->tcp_hdr->th_dport) == 443)
+    else if (dst_port == 443)
     {
         printf("This is HTTPS PORT IS 443!\n");
+    }
+    else
+    {
+        printf("Source port is %d\n", dst_port);
     }
 }
 
