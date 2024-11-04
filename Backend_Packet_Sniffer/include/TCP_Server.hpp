241012c474759a76123077283b20562f287cb9fd
omr50 <omrab4@gmail.com>
2024-11-03 21:44:29 -0500

    impolementing TCP communication between client server

--- Backend_Packet_Sniffer/include/TCP_Server.hpp
+++ Backend_Packet_Sniffer/include/TCP_Server.hpp
@@ -7,11 +7,12 @@ class TCP_Server
 {
 public:
     int port;
+    std::shared_ptr<boost::asio::ip::tcp::socket> client_socket;
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::acceptor acceptor_;
     TCP_Server(int port = 8000);
     void start_server();
     void start_accept();
-    void start_read(std::shared_ptr<boost::asio::ip::tcp::socket> socket_);
-    void start_write(std::shared_ptr<boost::asio::ip::tcp::socket> socket_, const std::string &message);
+    void start_read();
+    void start_write(const std::string &message);
 };
--- Backend_Packet_Sniffer/src/TCP_Server.cpp
+++ Backend_Packet_Sniffer/src/TCP_Server.cpp
@@ -6,20 +6,22 @@ TCP_Server::TCP_Server(int port) : io_context(), acceptor_(io_context, boost::as
 
 void TCP_Server::start_accept()
 {
-    auto socket_ = std::make_shared<boost::asio::ip::tcp::socket>(this->io_context);
-    this->acceptor_.async_accept(*socket_, [this, socket_](const boost::system::error_code &error)
+    this->client_socket = std::make_shared<boost::asio::ip::tcp::socket>(this->io_context);
+    this->acceptor_.async_accept([this](const boost::system::error_code &error)
                                  {
-            if (!error) {
-                start_read(socket_);
-            }
-            start_accept(); });
+                                     if (!error)
+                                     {
+                                         start_read();
+                                     }
+                                     // start_accept();
+                                 });
 }
 
-void TCP_Server::start_read(std::shared_ptr<boost::asio::ip::tcp::socket> socket_)
+void TCP_Server::start_read()
 {
     auto buf = std::make_shared<boost::asio::streambuf>();
 
-    boost::asio::async_read_until(*socket_, *buf, "\n", [this, socket_, buf](const boost::system::error_code &error, std::size_t)
+    boost::asio::async_read_until(*this->client_socket, *buf, "\n", [this, buf](const boost::system::error_code &error, std::size_t)
                                   {
         if (!error) {
             std::istream is(buf.get());
@@ -28,7 +30,7 @@ void TCP_Server::start_read(std::shared_ptr<boost::asio::ip::tcp::socket> socket
 
             std::cout << "Received: " << message << std::endl << "Now sending!" << std::endl;
             std::string write_message = "TESTING!\n"; 
-            this->start_write(socket_, write_message);
+            this->start_write(write_message);
         } });
 }
 
@@ -38,11 +40,11 @@ void TCP_Server::start_server()
     this->io_context.run();
 }
 
-void TCP_Server::start_write(std::shared_ptr<boost::asio::ip::tcp::socket> socket_, const std::string &message)
+void TCP_Server::start_write(const std::string &message)
 {
     auto msg = std::make_shared<std::string>(message);
 
-    boost::asio::async_write(*socket_, boost::asio::buffer(*msg), [socket_, msg](const boost::system::error_code &error, std::size_t)
+    boost::asio::async_write(*this->client_socket, boost::asio::buffer(*msg), [msg](const boost::system::error_code &error, std::size_t)
                              {
         if (!error) {
             std::cout << "Sent: " << *msg << std::endl;
