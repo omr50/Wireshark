8d9e948138baae01d18a6162468539d2d5f70767
omr50 <omrab4@gmail.com>
2024-11-08 23:23:56 -0500

    Comments for future

--- Backend_Packet_Sniffer/src/main.cpp
+++ Backend_Packet_Sniffer/src/main.cpp
@@ -12,6 +12,7 @@ int main()
 {
     std::shared_ptr<TCP_Server> server = std::make_shared<TCP_Server>(8000);
     std::thread server_thread(TCP_Server::server_thread, server);
+    // don't start the packet capture from here, rather from client is better
     Capture *packet_capture = new Capture("arp", server);
     packet_capture->start();
     return 0;
