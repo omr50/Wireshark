5aaf67bfb895265ed64d1aedc029267ccd4ec929
omr50 <omrab4@gmail.com>
2024-11-14 02:13:35 -0500

    header for ipv6

---
+++ Backend_Packet_Sniffer/include/PacketClasses/IPv6_Packet.hpp
@@ -0,0 +1,17 @@
+#include "./packet.hpp"
+#include <netinet/ip6.h>
+
+class IPv6_Packet : public Packet, public std::enable_shared_from_this<Packet>
+{
+public:
+    ip6_hdr *ip_hdr;
+
+    IPv6_Packet(const u_char *data, size_t length, timeval time_stamp);
+    ~IPv6_Packet() = default;
+    // should parse its own protocol specific data within the parser class
+    // make sure virtual functions are implemented in the concrete classes
+    void parse() override;
+    json print() override;
+    std::string print_source_addr();
+    std::string print_dest_addr();
+};
