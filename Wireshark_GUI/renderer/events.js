c4f01000a95c87675ddf6e11b26a9de26e6d75ed
Omr50 <omrab4@gmail.com>
2024-11-18 23:57:19 -0500

    Finished eth II dropdown

--- Wireshark_GUI/renderer/events.js
+++ Wireshark_GUI/renderer/events.js
@@ -1,24 +1,16 @@
-// function toggleSubItems(event, element) {
-//     event.stopPropagation();
-//     const subItems = element.querySelector('.sub-items');
-//     console.log(subItems.innerText)
-//     if (subItems.classList.contains('expanded')) {
-//         subItems.classList.remove('expanded');
-//         subItems.offsetHeight;
-//         // 
-//     } else {
-//         subItems.classList.add('expanded');
-//     }
-// }
 const { ipcRenderer } = require('electron');
 
 let packet_num = 1;
+let packets_info = [];
+
 const start_time = Date.now();
 console.log("CURRENT START TIME: ", start_time);
+
 function toggleSubItems(event, element) {
   event.stopPropagation();
 
   const subItems = element.querySelector('.sub-items');
+  const container = document.querySelector('.packet_details_container');
   const image = element.querySelector('.dropdown');
   if (subItems) {
     if (subItems.classList.contains('expanded')) {
@@ -62,15 +54,69 @@ function search(event, element) {
 }
 
 
+function renderDetailedInfo(packet_num, element) {
+  console.log(packets_info)
+  let detailedPacket = packets_info[packet_num]["detailed_info"];
+  // find the element where we can render the detailed packet info
+  let detailedProtocolElement = document.querySelector(".left-protocol-info");
+  // for every object set up a drop down.
+  // for simple string just display it in p tag
+  // later do a for each loop because the detailed packet will be an array of objects
+  // each object containing the layer
+  let list = `
+  <div class="packet_details_container">
+      <div class="item" onclick="toggleSubItems(event, this)">
+        <img class="dropdown" src="./dropdown.png" width="14px" height="14px">
+        ${detailedPacket["title"]} 
+        <div class="sub-items">
+            <div class="item" onclick="toggleSubItems(event, this)">
+                &emsp;&emsp;&emsp;<img class="dropdown" src="./dropdown.png" width="14px" height="14px">
+                ${detailedPacket["source"]["title"]} 
+                <div class="sub-items">
+                    <div>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${detailedPacket["source"]["LG"]} 
+                    </div>
+                    <div>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${detailedPacket["source"]["IG"]} 
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" onclick="toggleSubItems(event, this)">
+                &emsp;&emsp;&emsp;<img class="dropdown" src="./dropdown.png" width="14px" height="14px">
+                ${detailedPacket["Destination"]["title"]} 
+                <div class="sub-items">
+                    <div>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${detailedPacket["Destination"]["LG"]} 
+                    </div>
+                    <div>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${detailedPacket["Destination"]["IG"]} 
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" onclick="toggleSubItems(event, this)">
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${detailedPacket["type"]} 
+            </div>
+        </div>
+    </div> 
+  </div>
+  `
+  detailedProtocolElement.innerHTML = "";
+  detailedProtocolElement.innerHTML += list;
+}
+
+
 
 // Listen for the tcp-data event from the main process
 ipcRenderer.on('tcp-data', (event, data) => {
   console.log("THE TCP DATA IS ", data);
   let packet_info = data['packet_info'];
+  packets_info.push(data)
   let color_class = packet_info.protocol.toLowerCase(); 
   let exact_time = parseInt(packet_info.time) - start_time;
   let seconds = exact_time / 1000;
-  let new_row = `<tr class="${color_class}">
+  let new_row = `<tr class="${color_class}" onclick="renderDetailedInfo(${packet_num-1}, this)">
                   <td>${packet_num}</td>
                   <td>${seconds}</td>
                   <td>${packet_info.source}</td>
