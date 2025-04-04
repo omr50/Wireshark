48043e45d7050642874e1b86b2e12555b582a6d0
omr50 <omrab4@gmail.com>
2025-04-04 16:08:49 -0400

    added test udp server and client and Berkley Packet Filter args

--- Backend_Packet_Sniffer/src/main.cpp
+++ Backend_Packet_Sniffer/src/main.cpp
@@ -8,13 +8,22 @@
 #include "../include/Capture.hpp"
 #include "../include/TCP_Server.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
     std::shared_ptr<TCP_Server> server = std::make_shared<TCP_Server>(8000);
     std::thread server_thread(TCP_Server::server_thread, server);
     // don't start the packet capture from here, rather from client is better
     // client should be sent the available interfaces to choose from.
-    Capture *packet_capture = new Capture("arp || tcp || udp", server);
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " [filter_expr]\n";
+        std::cerr << "Example: " << argv[0] << " \"udp port 20020\"\n";
+        return 1;
+    }
+    std::string filter_expr;
+    filter_expr = argv[1];
+
+    Capture *packet_capture = new Capture(filter_expr, server);
     packet_capture->start();
     return 0;
 }
